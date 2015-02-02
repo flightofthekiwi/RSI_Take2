e46ae49a55a2ceafd76a29f21c1b2be0eeefff9f
flightofthekiwi <am2317@gmail.com>
2015-02-01 21:39:35 -0800

    Create rsi_onetime_break.py

---
+++ rsi_onetime_break.py
@@ -0,0 +1,9 @@
+#
+# This program will sleep for two hours and then play a song
+#
+import time
+import webbrowser
+
+time.sleep(7200)
+webbrowser.open("https://www.youtube.com/watch?v=yPYZpwSpKmA&spfreload=10")
+
